07d79f9ab9d34fb1bcb576efa5716a3f3e9fb7c3
sai <2603967@sit.singaporetech.edu.sg>
2026-09-04 15:15:54 +0800

    Roasted for age > 40

--- hello.py
+++ hello.py
@@ -7,3 +7,6 @@ print("++++++++++++++++++++++++++++++++++++")
 print("Username: ", username)
 print("Age: ", age)
 print("Category: ", category)
+
+if int(age)>40 and category.lower() == "fun":
+    print("You are old, what is fun for you?????")
