970f94b1c03b898c6391f5611dffd73070d91266
sai <2603967@sit.singaporetech.edu.sg>
2026-09-04 15:02:53 +0800

    Added variables

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("++++++++++++++++++++++++++++++++++++")
 print("Welcome here")
 print("My first post!")
 print("++++++++++++++++++++++++++++++++++++")
+
+username = "cl1che"
+bio = "Finding the meaning of life"
+followers = 0
+
+print("Username: ", username)
+print("Bio: ", bio)
+print("Followers: ", followers)
