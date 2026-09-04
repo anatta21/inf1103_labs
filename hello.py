71ecd507f1d9bd883d28ff8a079384bec83e5827
sai <2603967@sit.singaporetech.edu.sg>
2026-09-04 15:06:59 +0800

    test operators

--- hello.py
+++ hello.py
@@ -10,3 +10,14 @@ followers = 0
 print("Username: ", username)
 print("Bio: ", bio)
 print("Followers: ", followers)
+
+followers = 14
+
+followers += 20
+print("Day 1: ", followers)
+
+followers += 80
+print("Day 2: ", followers)
+
+followers -= 5
+print("Day 3: ", followers)
